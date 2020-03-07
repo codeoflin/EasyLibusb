3616a15c037a19892bdc5e59c68a197e4bb3549d
h@hhh.im <h@hhh.im>
2020-03-07 11:40:58 +0800

    1

--- main.c
+++ main.c
@@ -73,13 +73,12 @@ int get_device_descriptor(struct libusb_device_descriptor *dev_desc, struct user
 			break;
 		}
 	}
-
+	if(user_device->dev != NULL)rv = libusb_get_config_descriptor(user_device->dev,i,&conf_desc);
+	if(rv < 0) {printf("*** libusb_get_config_descriptor failed! \n");return -1;}
 	//return 0;
 	for (i = 0; i < dev_desc->bNumConfigurations; i++)
 	{
-		//return 0;
 		for (j = 0; j < conf_desc->bNumInterfaces; j++)
-			//return 0;
 			for (k = 0; k < conf_desc->interface[j].num_altsetting; k++)
 			{
 				//枚举找到端点描述符
